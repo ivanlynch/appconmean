d99e85d66724349791c22e62694daf068f01944d
Ivan Lynch <ivan.lynch@outlook.com.ar>
2016-09-24 22:12:34 -0300

    Creacion de data dummy, y conexion de Node con Angular

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,5 +2,12 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
+
+    $http.get('/contactlist').success(function(response){
+    	console.log("I got the data I requested");
+    	$scope.contactlist = response;
+    });
+
 }]);﻿
+
 
--- server.js
+++ server.js
@@ -3,8 +3,29 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(request, response){
-	response.send('Hello');
+app.get('/contactlist', function(req, res){
+	console.log("I received a GET request");
+
+	person1 = {
+		name: "Ivan",
+		email: "ivan@ivanlynch.com",
+		number: "(111) 111-1111"
+	};
+
+	person2 = {
+		name: "Ivan",
+		email: "ivan@ivanlynch.com",
+		number: "(111) 111-1111"
+	};
+
+	person3 = {
+		name: "Ivan",
+		email: "ivan@ivanlynch.com",
+		number: "(111) 111-1111"
+	};
+
+	var contactlist = [person1, person2, person3];
+	res.json(contactlist);
 });
 
 app.listen(3000, '192.168.0.22', function(){

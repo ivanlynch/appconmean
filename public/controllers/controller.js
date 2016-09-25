ef06574e8117e3ba8b420de7c4256baf8dbff5a3
Ivan Lynch <ivan.lynch@outlook.com.ar>
2016-09-25 03:25:54 -0300

    Configuracion de MongoDB, BodyParser y el post desde la vista

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,12 +1,33 @@
+//Declaramos el nombre del controlador
 var myApp = angular.module('myApp', []);
 
+//Declaramos el metodo controller de myApp
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
 
-    $http.get('/contactlist').success(function(response){
-    	console.log("I got the data I requested");
-    	$scope.contactlist = response;
-    });
+    var refresh = function(){
+    	//Pedimos el arreglo de objetos contactlist almacenado en el back-end
+    	$http.get('/contactlist').success(function(respuesta){
+	    	console.log("I got the data I requested");
+	    	//Obtiene los datos del form
+	    	$scope.contactlist = respuesta;
+	    	//Limpia el input
+	    	$scope.contact = "";
+    	});
+    };
+
+    //Refrescamos la pagina luego de obtener datos
+    refresh();
+
+    //Declaramos la funcion del boton Add Contact
+    $scope.addContact = function(){
+    	console.log($scope.contact);
+    	//Hacemos un post a contactlist y refrescamos
+    	$http.post('/contactlist', $scope.contact).success(function(respuesta){
+    		console.log(respuesta);
+    		refresh();
+    	});
+    };
 
 }]);﻿
 
--- server.js
+++ server.js
@@ -1,33 +1,47 @@
+//Hacemos require de express
 var express = require('express');
+var mongojs = require('mongojs');
+var bodyParser = require('body-parser');
+
+//Creamos una instancia de Express
 var app = express();
+//Definimos la DB, como primer parámetro es el nombre de la DB y el segundo es la colección
+var db = mongojs('contactlist', ['contactlist']);
 
+//Declaramos la carpeta Public donde vamos a almacenar las imágenes y scripts del front-end
 app.use(express.static(__dirname + "/public"));
+//Declaramos bodyParser que va a ser el encargado de parsear la solicitud para recibir solo el objeto
+app.use(bodyParser.json());
 
-app.get('/contactlist', function(req, res){
+//Le enviamos a la vista los documentos de la DB
+app.get('/contactlist', function(solicitud, respuesta){
 	console.log("I received a GET request");
 
-	person1 = {
-		name: "Ivan",
-		email: "ivan@ivanlynch.com",
-		number: "(111) 111-1111"
-	};
+	//Hacemos una busqueda en la coleccion y devolvemos un objeto JSON
+	db.contactlist.find(function(error, documento){
+		console.log(documento);
+		respuesta.json(documento);
+	})
 
-	person2 = {
-		name: "Ivan",
-		email: "ivan@ivanlynch.com",
-		number: "(111) 111-1111"
-	};
-
-	person3 = {
-		name: "Ivan",
-		email: "ivan@ivanlynch.com",
-		number: "(111) 111-1111"
-	};
-
-	var contactlist = [person1, person2, person3];
-	res.json(contactlist);
 });
 
+
+//Hacemos un post desde la vista
+app.post('/contactlist', function(solicitud, respuesta){
+	console.log(solicitud.body);
+
+	//Guardamos los datos que recibimos de la vista
+	db.contactlist.insert(solicitud.body, function(error, documento){
+		if(error){
+			console.log(error);
+		}else{
+			//Enviamos el documento que recibimos a mongoDB
+			respuesta.json(documento);
+		}
+	});
+});
+
+//Declaramos el servidor puerto e ip
 app.listen(3000, '192.168.0.22', function(){
 	console.log('Server running on port 3000');
 })
